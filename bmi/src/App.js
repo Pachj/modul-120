f5f9a4ad3eedf4f5bf4e345e1bdaa92f8d364530
Henry Joerg <henry.joerg@gmail.com>
2021-09-13 14:32:55 +0200

    colors for bmi added

--- bmi/src/App.js
+++ bmi/src/App.js
@@ -1,32 +1,53 @@
 import './App.css';
-import {useState} from 'react';
-
+import { useState } from 'react';
+import React from 'react';
 
 function App() {
   const [weight, setWeight] = useState(0);
   const [size, setSize] = useState(0);
-  const [bmi, setBmi] = useState(0);
+  const [bmi, setBmi] = useState('Bitte geben Sie Ihr Gewicht und Ihre Grösse ein.');
+  const [backgroundColor, setBackgroundColor] = useState('transparent');
 
   const calculateBMI = () => {
-    setBmi(weight / Math.sqrt(size));
-  }
+    // round to 2 decimals
+    setBmi(Math.round((weight / size / size) * 10000 * 100) / 100);
+  };
+
+  React.useEffect(() => {
+    if (bmi <= 18.5) {
+      setBackgroundColor('red');
+    } else if (bmi > 18.5 && bmi <= 24.9) {
+      setBackgroundColor('green');
+    } else if (bmi > 24.9) {
+      setBackgroundColor('red');
+    } else {
+      setBackgroundColor('transparent');
+    }
+  }, [bmi]);
 
   return (
     <div className="App">
-      <div>
-        <label>
-          Ihr Gewicht (kg):
-          <input value={weight} onChange={e => setWeight(e.target.value)} type={'number'}/>{' '}
-          kg
-        </label>
-        <label>
-          Ihre Körpergrösse(cm):
-          <input value={size} onChange={e => setSize(e.target.value)} type={'number'}/>{' '}
-          cm 
-        </label>
+      <div className="box-container">
+        <h1>BMI Calculator</h1>
+        <div className="inner-container">
+          <label>
+            Ihr Gewicht (kg):
+            <input value={weight} onChange={(e) => setWeight(e.target.value)} type={'number'} /> kg
+          </label>
+        </div>
+        <div className="inner-container">
+          <label>
+            Ihre Körpergrösse(cm):
+            <input value={size} onChange={(e) => setSize(e.target.value)} type={'number'} /> cm
+          </label>
+        </div>
         <button onClick={calculateBMI}>Berechnen</button>
+        <div id="output-container" className="inner-container">
+          <p id="output" style={{ backgroundColor: backgroundColor }}>
+            {bmi}
+          </p>
+        </div>
       </div>
-      <p>{bmi}</p>
     </div>
   );
 }
