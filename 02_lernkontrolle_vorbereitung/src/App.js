0df2b57c71abcb0744645fda38a8876d36f4106f
Henry Joerg <henry.joerg@gmail.com>
2021-10-29 13:58:11 +0200

    eslint errors fixed

--- 02_lernkontrolle_vorbereitung/src/App.js
+++ 02_lernkontrolle_vorbereitung/src/App.js
@@ -1,62 +1,94 @@
-import './App.css';
-import {useState} from "react";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Output from "./Output";
+import React, { useState } from 'react'
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Output from './Output'
 
 function App() {
-  const [textState, setTextState] = useState('');
-  const [radioState, setRadioState] = useState('');
-  const [checkboxCheckedState, setCheckboxCheckedState] = useState(false);
-  const [showOutput, setShowOutput] = useState(false);
+  const [textState, setTextState] = useState('')
+  const [radioState, setRadioState] = useState('')
+  const [checkboxCheckedState, setCheckboxCheckedState] = useState(false)
+  const [showOutput, setShowOutput] = useState(false)
 
   const submitHandler = (e) => {
-    setShowOutput(!showOutput);
-    e.preventDefault();
+    setShowOutput(!showOutput)
+    e.preventDefault()
   }
 
   return (
     <div className="App">
-      <div className={'form'}>
+      <div className="form">
         <form onSubmit={submitHandler}>
-          <div className={'form-group'}>
-            <label>Text Input
-              <input className={'form-control'} required={true} type={'text'} value={textState} onChange={(event) => {
-                setTextState(event.target.value)
-              }}/>
+          <div className="form-group">
+            <label>
+              Text Input
+              <input
+                className="form-control"
+                required
+                type="text"
+                value={textState}
+                onChange={(event) => {
+                  setTextState(event.target.value)
+                }}
+              />
             </label>
           </div>
-          <div className={'form-check'}>
-            <label className={'form-check-label'}>
-              <input className={'form-check-input'} type={'radio'} value={'radio1'} checked={radioState === 'radio1'}
-                     onChange={(event) => {
-                       setRadioState(event.target.value)
-                     }}/>
+          <div className="form-check">
+            <label className="form-check-label">
+              <input
+                className="form-check-input"
+                type="radio"
+                value="radio1"
+                checked={radioState === 'radio1'}
+                onChange={(event) => {
+                  setRadioState(event.target.value)
+                }}
+              />
               Radio 1
             </label>
           </div>
-          <div className={'form-check'}>
-            <label className={'form-check-label'}>
-              <input className={'form-check-input'} type={'radio'} value={'radio2'} checked={radioState === 'radio2'}
-                     onChange={(event) => {
-                       setRadioState(event.target.value)
-                     }}/>
+          <div className="form-check">
+            <label className="form-check-label">
+              <input
+                className="form-check-input"
+                type="radio"
+                value="radio2"
+                checked={radioState === 'radio2'}
+                onChange={(event) => {
+                  setRadioState(event.target.value)
+                }}
+              />
               Radio 2
             </label>
           </div>
-          <div className={'form-check'}>
-            <label className={'form-check-label'}>Checkbox
-              <input className={'form-check-input'} type={'checkbox'} checked={checkboxCheckedState}
-                     onChange={(event) => {
-                       setCheckboxCheckedState(!checkboxCheckedState)
-                     }}/>
+          <div className="form-check">
+            <label className="form-check-label">
+              Checkbox
+              <input
+                className="form-check-input"
+                type="checkbox"
+                checked={checkboxCheckedState}
+                onChange={() => {
+                  setCheckboxCheckedState(!checkboxCheckedState)
+                }}
+              />
             </label>
           </div>
-          <button className={'btn btn-primary'} type={'submit'}>Submit</button>
+          <button className="btn btn-primary" type="submit">
+            Submit
+          </button>
         </form>
       </div>
-      {showOutput ? <Output textState={textState} radioState={radioState} checkboxCheckedState={checkboxCheckedState} /> : ''}
+      {showOutput ? (
+        <Output
+          textState={textState}
+          radioState={radioState}
+          checkboxCheckedState={checkboxCheckedState}
+        />
+      ) : (
+        ''
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- 02_lernkontrolle_vorbereitung/src/Output.jsx
+++ 02_lernkontrolle_vorbereitung/src/Output.jsx
@@ -1,7 +1,8 @@
+import React from 'react'
+
 export default function Output(props) {
-  console.log(props.checkboxCheckedState)
   return (
-    <div className={'outputContainer'}>
+    <div className="outputContainer">
       <p>TextInput: {props.textState}</p>
       <p>RadioSelection: {props.radioState}</p>
       <p>Checkbox: {props.checkboxCheckedState === true ? 'True' : 'False'}</p>
